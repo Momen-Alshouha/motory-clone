f0d7f4724e59004ccfa04ec0fc6200d91a6d61e0
momen_alshouha <momenalshouha@outlook.com>
2024-01-23 15:07:36 +0300

    working on about section

--- src/app/modules/home/about/about.component.ts
+++ src/app/modules/home/about/about.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  imageURL : string = 'https://stg-pre-live2-ksa.motory.com/public/assets/images/home/intermediary-platform.svg';
+  title : string = 'Title';
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 }
