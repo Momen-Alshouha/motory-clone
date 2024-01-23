1493537145237e8853b2b45c750221d4da1bf1f7
Fathi Abualrub <fathia@motory.com>
2024-01-23 12:45:10 +0300

    fix layout issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./modules/home/home.component";
+import {LayoutComponent} from "./shared/modules/layout/layout.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: LayoutComponent,
     children: [
+      { path: '', component: HomeComponent }
     ],
   },
 ];
